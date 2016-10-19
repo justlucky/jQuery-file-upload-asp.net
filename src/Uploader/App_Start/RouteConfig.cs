701e6c6076ac16ee59b1a8a83b42b08f29614f70
i.yilmaz <i.yilmaz@ventusbilisim.com.tr>
2016-10-19 19:41:55 +0300

    rename default controller & transform to apicontroller

--- src/Uploader/App_Start/RouteConfig.cs
+++ src/Uploader/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Uploader
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Upload", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
